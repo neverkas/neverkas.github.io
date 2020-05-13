a8ece0680b5bc139bf7db3fe114eb16e98e76722
jelou <emmanuel.lazarte.012@gmail.com>
2020-05-12 22:52:05 -0300

    Se agrega nuevo template

---
+++ utn/manu-theme/custom.js
@@ -0,0 +1,16 @@
+//jQuery.noConflict();
+
+//(function( $ ) {
+//    $(document).load(function () {
+        $.getScript('../themes/styles/lib/js/bootstrap.min.js', function() {
+            console.log("bootstrap loaded");
+        });
+        $.getScript('../themes/styles/lib/js/stickytableheaders.min.js', function() {
+            console.log("stickytableheaders loaded");
+        });
+        $.getScript('../themes/styles/readtheorg/js/readtheorg.js', function() {
+            console.log("stickytableheaders loaded");
+        });
+//    });       
+//})( jQuery );
+
